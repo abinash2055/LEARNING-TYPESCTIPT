be3f0f22d38538ec9a7fb04eba1c34391dec9c7a
Abinash Nath Pandey <abinashnathpandey@gmail.com>
2024-11-06 10:53:53 +0545

    Day 1

---
+++ Day 1/app.js
@@ -0,0 +1,10 @@
+"use strict";
+var a = 12;
+function abcd() {
+    console.log(a);
+    let x = 33;
+    let y = 67;
+    console.log(x);
+    console.log(y);
+}
+var b = 34;
---
+++ Day 1/app.ts
@@ -0,0 +1,11 @@
+var a = 12;
+
+function abcd() {
+  console.log(a);
+  let x = 33;
+  let y = 67;
+  console.log(x);
+  console.log(y);
+}
+
+var b = 34;
