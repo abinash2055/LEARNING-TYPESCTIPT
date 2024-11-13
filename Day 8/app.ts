8c71336b29df516bc48a15213455367447df04bd
Abinash Nath Pandey <abinashnathpandey@gmail.com>
2024-11-13 18:32:12 +0545

    Assertion and casting

---
+++ Day 8/app.ts
@@ -0,0 +1,21 @@
+// Type Assertion --> to tell TypeScript as particular things that has a certain type as value .
+// we use it when coder know more details of that typeof data than TypeScript.
+
+let a = Number("342"); // string to number
+console.log(typeof a); // its call as type assertion
+console.log(a);
+
+// Type Casting
+let b = String(456456); // number to string
+console.log(typeof b); // call as type assertion or type casting
+console.log(b);
+
+// Non-Null  assertion operator
+let d: null | undefined | boolean | string;
+
+// if you are sure its value be string
+d = "its string than other type";
+
+// to be sure that its value is string and get string property only
+d!.slice(3, 9);
+console.log(d);
